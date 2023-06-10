fe36b6dda0a218c9e50568c6c67329fb72fd6a8f
Bartlomiej Lanczyk <bartlomiejl@backbase.com>
2023-06-10 09:57:41 +0200

    Initial commit

---
+++ SimpleDeepLinks/SimpleDeepLinks/SimpleDeepLinksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleDeepLinksApp.swift
+//  SimpleDeepLinks
+//
+//  Created by Bartlomiej Lanczyk on 10/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleDeepLinksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
